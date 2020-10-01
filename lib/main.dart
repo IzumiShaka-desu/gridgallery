ec28da363f017cf43c71202ba0bf96683536ed51
sesakaaji19137 <sesaka.aji19137@student.unsika.ac.id>
2020-10-01 16:55:22 +0700

    melakukan pemisahan kode

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:gallery/contstant.dart';
+import 'package:gallery/home.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meet_network_image/meet_network_image.dart';
 
-import 'detail.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,42 +19,5 @@ class MyApp extends StatelessWidget {
       home: Home(),
       debugShowCheckedModeBanner: false,
     );
-  }
-}
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('Gallery')),
-        body: Container(
-          child: Container(
-              child: GridView.builder(
-                  itemCount: listImage.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3),
-                  itemBuilder: (context, index) => Hero(
-                        tag: "photo$index",
-                        child: GestureDetector(
-                          onTap: () => Navigator.of(context).push(
-                              MaterialPageRoute(
-                                  builder: (context) => Detail(tag:"photo$index",photo:listImage[index]))),
-                          child: Container(
-                              padding: EdgeInsets.all(10),
-                              child: MeetNetworkImage(
-                                  imageUrl: listImage[index].url,
-                                  fit: BoxFit.cover,
-                                  loadingBuilder: (context) => Center(
-                                        child: SizedBox(
-                                            width: 15,
-                                            height: 15,
-                                            child: CircularProgressIndicator()),
-                                      ),
-                                  errorBuilder: (context, _) => Center(
-                                        child: Icon(Icons.error_outline),
-                                      ))),
-                        ),
-                      ))),
-        ));
   }
 }
